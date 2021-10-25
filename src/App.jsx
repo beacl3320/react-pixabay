d066100ab59fd27ca8d3aac681f7125488b14ae6
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-10-25 14:26:11 +0200

    FEAT: 'clean app, ready to start develop'

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="container">
+     <div className="h-100 p-5 text-white bg-dark rounded-3">
+       <p className="lead text-center ">Buscador de Imágenes</p>
+     </div>
+     <button class="btn btn-outline-light w-100" type="button">Buscar</button>
+    </div>
+  );
+}
+
+export default App;
